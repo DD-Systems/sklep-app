906827335ac917dee98bf6bbf9892d7d75ae89da
Daniel Warcino <hrabia30@gmail.com>
2026-05-11 12:43:12 +0200

    Connect storefront to Cloudflare order API

--- shop-config.js
+++ shop-config.js
@@ -1,4 +1,4 @@
 window.SHOP_CONFIG = window.SHOP_CONFIG || {
-  orderApiUrl: "",
+  orderApiUrl: "https://sklep-app-api.sklepapp2026-api.workers.dev",
   adminPasswordHint: "Ustaw własne hasło administratora w backendzie."
 };
